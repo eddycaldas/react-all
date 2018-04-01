84b96a53ffcc4f2f231ea0d6f59b0fd06a2fe063
Eddy Caldas <eddycaldas@live.com>
2018-04-01 16:21:23 -0600

    count

---
+++ count/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Count from './components/Count';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Count />
+      </div>
+    );
+  }
+}
+
+export default App;
--- indecision-app/src/components/options/Options.js
+++ indecision-app/src/components/options/Options.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import Option from './option/Option';
 
 class Options extends Component {
+    constructor(props) {
+        super(props);
+        this.handleRemoveAll = this.handleRemoveAll.bind(this);
+    }
 handleRemoveAll() {
-    alert ("removed")
+    console.log(this.props.options)
+    // alert ("removed")
 }
 
   render() {
